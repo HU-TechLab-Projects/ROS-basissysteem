3db91fb3dc140aff3767f6121b91410fe17498df
Nerinai <david.dm.dejong@student.hu.nl>
2019-07-18 14:17:27 +0200

    added auto sychronization to poc

--- firstpoc/Native/ledsmainpcside.cpp
+++ firstpoc/Native/ledsmainpcside.cpp
@@ -1,41 +1,67 @@
 #include "ledsmainpcside.hpp"
-
+#include <random>
 int ledsmainpcside(){
   auto comm = nanocom<>();
-
-  command command_msg = command_init_zero;
-  ack_nak ack_nak_msg = ack_nak_init_zero;
-  sum_rpc sum_rpc_msg = sum_rpc_init_zero;
-  single_int rpc_response = single_int_init_zero;
   char msg [] = "sum\0";
-  strncpy(command_msg.command, msg, sizeof(command_msg.command));
+  char led [] = "led\0";
+  while(true){
+    std::random_device generator;
+    std::uniform_int_distribution<int> distribution(1,1000);
 
-  for(uint32_t i = 0; i < sizeof(command_msg.command); i++){
-    std::cout << command_msg.command[i];
+    command command_msg = command_init_zero;
+    ack_nak ack_nak_msg = ack_nak_init_zero;
+    sum_rpc sum_rpc_msg = sum_rpc_init_zero;
+    single_int rpc_response = single_int_init_zero;
+
+
+    strncpy(command_msg.command, msg, sizeof(command_msg.command));
+
+    // for(uint32_t i = 0; i < sizeof(command_msg.command); i++){
+    //   std::cout << command_msg.command[i];
+    // }
+    // std::cout << std::endl;
+
+    comm.send_message(&command_msg, command_fields);
+    comm.receive_message(&ack_nak_msg, ack_nak_fields);
+    if(!ack_nak_msg.ack){
+      std::cout << "sum command did not get an ack" << std::endl;
+      continue;
+    }
+    sum_rpc_msg.a = distribution(generator);
+    sum_rpc_msg.b = distribution(generator);
+
+    // std::cout << "sending sum of numbers: "
+    //           << sum_rpc_msg.a
+    //           << " and "
+    //           << sum_rpc_msg.b
+    //           << std::endl;
+
+    comm.send_message(&sum_rpc_msg, sum_rpc_fields);
+    comm.receive_message(&rpc_response, single_int_fields);
+
+    // std::cout << "the answer is "
+    //           << rpc_response.response
+    //           << std::endl;
+
+    rpc_response.response = rpc_response.response % 5;
+    strncpy(command_msg.command, led, sizeof(command_msg.command));
+
+    comm.send_message(&command_msg, command_fields);
+    comm.receive_message(&ack_nak_msg, ack_nak_fields);
+    if(!ack_nak_msg.ack){
+      std::cout << "led command did not get an ack" << std::endl;
+      continue;
+    }
+
+    comm.send_message(&rpc_response, single_int_fields);
+    comm.receive_message(&ack_nak_msg, ack_nak_fields);
+    if(!ack_nak_msg.ack){
+      std::cout << "set led command did not get an ack" << std::endl;
+      continue;
+    }
+
+    // std::cout << "led: " << (rpc_response.response +1) << " should be on now" << std::endl;
+    // usleep(1000000);
   }
-  std::cout << std::endl;
-
-  comm.send_message(&command_msg, command_fields);
-  while(!comm.receive_message(&ack_nak_msg, ack_nak_fields));
-
-  sum_rpc_msg.a = 5;
-  sum_rpc_msg.b = 4;
-
-  std::cout << "sending sum of numbers: "
-            << sum_rpc_msg.a
-            << " and "
-            << sum_rpc_msg.b
-            << std::endl;
-
-  comm.send_message(&sum_rpc_msg, sum_rpc_fields);
-  while(!comm.receive_message(&rpc_response, single_int_fields));
-
-  std::cout << "the answer is "
-            << rpc_response.response
-            << std::endl;
-
-
-
-
   return 0;
 }
